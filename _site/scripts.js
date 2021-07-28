e3ef2ef41f7f5071786e99365a01109ac1406a02
Luke <77371718+ldalton02@users.noreply.github.com>
2021-07-27 22:32:08 -0700

    wilding out

--- _site/scripts.js
+++ _site/scripts.js
@@ -6,6 +6,7 @@ const snapLoad = (element) => {
 const bars = $('.progress-bar');
 
 $(document).ready(function () {
+  console.log('hello');
   $('body').scrollspy({
     target: '.navbar'
   })
@@ -36,4 +37,40 @@ $(document).ready(function () {
     startDelay: 1000,
   });
 
+  var button = document.getElementsByClassName('collapsible');
+  for (each in button) {
+    each.onclick = function() {
+      let content = document.getElementById('collapsible1');
+      content.style.display = 'block';
+      let height = content.scrollHeight + 'px';
+      this.classList.toggle('active');
+      if (content.classList.contains('is-visible')) {
+        content.classList.remove('is-visible');
+      } else {
+        content.classList.add('is-visible');
+      }
+    }
+  }
+  
+
+
+
 });
+
+
+/*
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+*/
